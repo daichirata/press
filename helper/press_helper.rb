291c023c728fb6820b32c8209733d70e883aeceb
Dach-h <bunny.hop.md@gmail.com>
2011-10-31 12:22:39 +0900

    add thumbnail and read_more

---
+++ helper/press_helper.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+module Lokka
+  module Helpers
+    def read_more(body, link)
+      if more = body.index("[More]")
+        body = body[0..(more - 1)]
+        body << %Q|<p class="read-more"><a href="#{link}">続きを読む</a></p>|
+      end
+      return body
+    end
+
+    def rm_read_more(body)
+      body.gsub!(/\[More\]/, "")
+    end
+  end
+end
+
