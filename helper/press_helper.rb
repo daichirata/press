98d669620ed291d0646cb7ed8e5ecada1cec6066
Dach-h <bunny.hop.md@gmail.com>
2011-11-02 16:33:34 +0900

    全体的にスタイルの調整

--- helper/press_helper.rb
+++ helper/press_helper.rb
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 module Lokka
   module Helpers
-    def read_more(body, link)
+    def press_bread_crumb
+      return if @bread_crumbs.size == 1
+      @bread_crumbs[0..-2].inject('<ol>') do |html,bread|
+        html += "<li><a href=\"#{bread[:link]}\">#{bread[:name]}</a></li>"
+      end + "<li>#{@bread_crumbs[-1][:name]}</li></ol>"
+    end
+
+    def press_read_more(body, link)
       if more = body.index("[More]")
         body = body[0..(more - 1)]
         body << %Q|<p class="read-more"><a href="#{link}">続きを読む</a></p>|
@@ -9,8 +16,23 @@ module Lokka
       return body
     end
 
-    def rm_read_more(body)
+    def press_remove_more(body)
       body.index("[More]") ? body.sub!(/\[More\]/, "") : body
+    end
+
+    def press_pager(entry, option = {})
+      prev_entry, next_entry = Entry(entry.id - 1), Entry(entry.id + 1)
+      html =  "<nav class=#{option[:class]}>"
+      html <<   "<ul>"
+      html <<     "<li class='next'>"
+      html <<       "<a class='next-link' href=#{next_entry.link}>#{next_entry.title}</a>" if next_entry
+      html <<     "</li>"
+      html <<     "<li class='previous'>"
+      html <<       "<a class='previous-link' href=#{prev_entry.link}>#{prev_entry.title}</a>" if prev_entry
+      html <<     "</li>"
+      html <<   "</ul>"
+      html << "</nav>"
+      html.html_safe
     end
   end
 end
